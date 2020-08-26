2b9c248a93b4ef769844a1d4e8ceddff58ed9eef
Vincent Servio <vincentservio@gmail.com>
2020-08-26 21:12:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+const form = document.querySelector("#create-task-form")
+const list = document.getElementById("tasks")
+
+
+
+
+
+const getText = event => {
+  event.preventDefault()
+const input = document.getElementById("new-task-description").value
+
+
+
+  list.innerHTML += `
+  <li>${input} <button>X</button>
+  </li>`
+
+    
+}
+
+form.addEventListener("submit", getText)
+
+
+
+
+
+
+
+
+
+
+
 });
